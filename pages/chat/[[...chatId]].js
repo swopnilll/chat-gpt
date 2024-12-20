043bfaa629052674b1b7e8fdc2967bf01c03b76c
swopnilll <swopnilacharya@gmail.com>
2024-12-21 09:47:21 +1100

    first commit

---
+++ pages/chat/[[...chatId]].js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Chat Page</title>
+      </Head>
+      <h1>Welcome to the Chat page</h1>
+    </div>
+  );
+}
